f29db7ff836a7961a91dff866e83804e9eee3809
duanguanjun <duanguanjun@yeah.net>
2019-12-31 05:04:37 +0800

    init

---
+++ precache-manifest.4e31d89ad24f968f1e1644687858149f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfdca4c3a92202820893a3088d44bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8aba73ec67e7d863c5",
+    "url": "/static/css/main.9fc4fdf3.chunk.css"
+  },
+  {
+    "revision": "a0603507f234df1bae5b",
+    "url": "/static/js/2.fc1fe354.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fc1fe354.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a8aba73ec67e7d863c5",
+    "url": "/static/js/main.6bb9b3fc.chunk.js"
+  },
+  {
+    "revision": "cc72382836bc84597c27",
+    "url": "/static/js/runtime-main.35d81fc7.js"
+  }
+]);
